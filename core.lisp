7834d2344dc54418197ebc2810510a7425b4ea01
Sean Whitton <spwhitton@spwhitton.name>
2021-02-14 12:45:06 -0700

    fix use of non-lexical #'eval
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- core.lisp
+++ core.lisp
@@ -376,10 +376,7 @@ specification."
 			   (not (member (symbol-name first)
 					'("UNAPPLY")
 					:test #'string=)))
-		       ;; TODO this eval is not what we want because we want
-		       ;; the lexical environment in which the macro call
-		       ;; occurs, not the null lexical environment!
-		       `(cons ',first (mapcar #'eval ',rest))
+		       `(list ',first ,@rest)
 		       `(list ,@(mapcar #'make-eval-propspec form)))))))
     `(make-instance
       'propspec
