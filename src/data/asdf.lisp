06f65655c006ab41e103ed17b1da8df298fad67d
Sean Whitton <spwhitton@spwhitton.name>
2021-02-16 23:39:24 -0700

    normalise file headers and some comments
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- src/data/asdf.lisp
+++ src/data/asdf.lisp
@@ -1,3 +1,20 @@
+;;; Consfigurator -- Lisp declarative configuration management system
+
+;;; Copyright (C) 2021  Sean Whitton <spwhitton@spwhitton.name>
+
+;;; This file is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3, or (at your option)
+;;; any later version.
+
+;;; This file is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package :consfigurator.data.asdf)
 
 (defmethod register-data-source ((type (eql :asdf)) &key)
--- src/data/pgp.lisp
+++ src/data/pgp.lisp
@@ -1,6 +1,23 @@
+;;; Consfigurator -- Lisp declarative configuration management system
+
+;;; Copyright (C) 2021  Sean Whitton <spwhitton@spwhitton.name>
+
+;;; This file is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3, or (at your option)
+;;; any later version.
+
+;;; This file is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package :consfigurator.data.pgp)
 
-;;;; Simple PGP-encrypted file source of prerequisite data
+;; Simple PGP-encrypted file source of prerequisite data
 
 ;; We provide an implementation of REGISTER-DATA-SOURCE and functions for the
 ;; user to call at the REPL to add pieces of data, see what's there, etc.  (a
