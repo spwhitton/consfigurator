478194b9593b865286790ff24b1172abe9233f1b
Sean Whitton <spwhitton@spwhitton.name>
2021-02-16 23:31:37 -0700

    use writefile in file:file-has-content
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- src/property/file.lisp
+++ src/property/file.lisp
@@ -2,13 +2,13 @@
 
 (defprop file-has-content :posix (path lines)
   "Ensure there is a file at PATH whose lines are the elements of LINES."
-  (:apply (connection-writefile path (unlines lines))))
+  (:apply (writefile path (unlines lines))))
 
 (defprop file-contains-lines :posix (path lines)
   "Ensure there is a file at PATH containing each of LINES."
   (:apply (let ((new-lines (copy-list lines))
-		(existing-lines (lines (connection-readfile path))))
+		(existing-lines (lines (readfile path))))
 	    (loop for existing-line in existing-lines
 		  do (setq new-lines (delete existing-line new-lines)))
-	    (connection-writefile path (unlines
-					(nconc existing-lines new-lines))))))
+	    (writefile path (unlines
+			     (nconc existing-lines new-lines))))))
