1bf6955369fe47a0cc171b69466e5b6d5b96151c
Sean Whitton <spwhitton@spwhitton.name>
2021-03-04 14:59:19 -0700

    replace dummy macro definitions with some Elisp
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

---
+++ indentation.el
@@ -0,0 +1,5 @@
+;; if you eval these forms in Emacs you'll get correct indentation in
+;; unevaluated property application specifications
+
+(put 'deploys. 'common-lisp-indent-function '(4 4 2))
+(put 'deploys-these. 'common-lisp-indent-function '(4 4 2))
