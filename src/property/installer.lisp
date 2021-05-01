d2f5c3fa881329dcf420b792b3b1c5f68257d91c
Sean Whitton <spwhitton@spwhitton.name>
2021-04-30 17:38:08 -0700

    comments
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- src/property/installer.lisp
+++ src/property/installer.lisp
@@ -50,7 +50,14 @@ Also update the fstab and crypttab, and try to install a bootloader."
           :systems nil
           :propspec
           '(eseqprops
+            ;; This will overwrite any custom mount options, etc., with values
+            ;; from VOLUMES.  Possibly it would be better to use a property
+            ;; which only updates the fs-spec field.  However, given that
+            ;; VOLUMES ultimately comes from the volumes the user has declared
+            ;; for the host, it is unlikely there are other properties setting
+            ;; mount options etc. which are in conflict with VOLUMES.
             (fstab:entries-for-opened-volumes)
+            ;; Debian and derivatives add this line.
             (file:lacks-lines "/etc/fstab" "# UNCONFIGURED FSTAB FOR BASE SYSTEM"))))
        ;; TODO Update /etc/crypttab
        ;; TODO Install bootloader
