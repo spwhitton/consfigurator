720ee21a0905d36cf29af49ce8b6d572296dddb6
Sean Whitton <spwhitton@spwhitton.name>
2021-10-06 14:38:01 -0700

    :SETUID: error when OSICAT:USER-INFO fails
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- src/connection/setuid.lisp
+++ src/connection/setuid.lisp
@@ -24,7 +24,8 @@
   (unless (and (lisp-connection-p) (zerop (nix:geteuid)))
     (error "~&SETUIDing requires a Lisp image running as root"))
   (informat 1 "~&SETUIDing to ~A" to)
-  (let* ((ent (osicat:user-info to))
+  (let* ((ent (or (osicat:user-info to)
+                  (failed-change "~&Could not look up user info for ~A." to)))
 	 (xdg-cache-home
            (ensure-directory-pathname
             (stripln
