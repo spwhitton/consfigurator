9aac95464535216174b35e59704ae5eece3b25d7
Sean Whitton <spwhitton@spwhitton.name>
2022-06-24 14:17:20 -0700

    user.lisp: update copyright years
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- src/property/user.lisp
+++ src/property/user.lisp
@@ -1,7 +1,7 @@
 ;;; Consfigurator -- Lisp declarative configuration management system
 
-;;; Copyright (C) 2021  David Bremner <david@tethera.net>
-;;; Copyright (C) 2021  Sean Whitton <spwhitton@spwhitton.name>
+;;; Copyright (C) 2021-2022  David Bremner <david@tethera.net>
+;;; Copyright (C) 2021-2022  Sean Whitton <spwhitton@spwhitton.name>
 
 ;;; This file is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
