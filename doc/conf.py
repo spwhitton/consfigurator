0b694bfd8cbb71e64fd852f961aa893f2939e8da
Sean Whitton <spwhitton@spwhitton.name>
2023-06-12 08:49:30 +0100

    release Consfigurator 1.3.1 (-1 to Debian unstable)
    Signed-off-by: Sean Whitton <spwhitton@spwhitton.name>

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2015-2018, 2020-2023, Sean Whitton, 2021-2022 David Bremner'
 author = 'Sean Whitton'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.3.1'
 
 
 # -- General configuration ---------------------------------------------------
